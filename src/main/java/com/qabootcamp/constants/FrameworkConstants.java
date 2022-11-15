777f32e4681765800f9ec36cef3dd5bf203e69c4
Tushar Varma <tushar.varma@Tushars-MacBook-Pro.local>
2022-11-15 09:15:49 +0530

    Initial commit

--- src/main/java/com/qabootcamp/constants/FrameworkConstants.java
+++ src/main/java/com/qabootcamp/constants/FrameworkConstants.java
@@ -1,2 +1,8 @@
 package com.qabootcamp.constants;public class FrameworkConstants {
+    private static final String CHROMEDRIVERPATH =  System.getProperty("user.dir")+"/src/test/resources/executables/chromedriver";
+
+    public static String getChromeDriverPath()
+    {
+        return CHROMEDRIVERPATH;
+    }
 }
